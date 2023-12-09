623bd5ffb70daed4af8867151a940b7e9775535d
loy343 <cute_as_ducks@yahoo.com>
2023-12-08 21:38:34 -0600

    styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
  
 export default function Home() {
@@ -13,10 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <div>Hello Worlds</div>
-      </main>
+      
+      {/* Background */}
+      <GradientBackgroundCon></GradientBackgroundCon>
 
 
     </>
